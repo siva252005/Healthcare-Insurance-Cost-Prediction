2c0122366990c161dd77fcd3c04f24077eb1db0f
siva <msdsiva762@gmail.com>
2026-07-20 11:23:50 +0530

    project structure created

---
+++ src/utils/helper.py
@@ -0,0 +1,19 @@
+# Virtual Environment
+.venv/
+
+# Python Cache
+__pycache__/
+*.pyc
+
+# Jupyter Notebook Checkpoints
+.ipynb_checkpoints/
+
+# VS Code
+.vscode/
+
+# macOS
+.DS_Store
+
+
+# Environment Variables
+.env
